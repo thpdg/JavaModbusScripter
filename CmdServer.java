173eeecabb28200b02354fc4778f9a4b1ba1aa87
thpdg <githubuser@hullbreach.com>
2021-04-12 08:49:56 -0400

    Added copyright header

--- CmdServer.java
+++ CmdServer.java
@@ -1,3 +1,9 @@
+//  
+// Copyright (c) THPDG. All rights reserved.  
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.  
+//  
+
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
